8eb63ecf3db19bd46f1eb63d2942908316140157
siennaJeong <sienna4342@gmail.com>
2020-08-26 12:47:00 +0900

    [ADD] 200826 - Kakao 로그인 Api 추가 [FIX] 200826 - Kakao 로그인 Api 사용중 SDK Version Error 로 인해 안드로이드, ios SDK Version 변경

--- lib/view/ui/splash.dart
+++ lib/view/ui/splash.dart
@@ -1,19 +1,25 @@
 import 'package:deukki/common/utils/routeUtil.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Splash(),
-      routes: routes,
-    )
-);
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Splash(),
+        routes: routes,
+      )
+  );
+}
 
 class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("hi"),
+    return Scaffold(
+      body: Container(
+        child: Text("hello"),
+      ),
     );
   }
 }
