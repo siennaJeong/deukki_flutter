58ecb13147319855a537ecf6dd9a7345c655b3f7
siennaJeong <sienna4342@gmail.com>
2020-11-12 10:25:58 +0900

    [ADD] 2001112 - Setting 화면에서 업데이트 버튼 숨김.

--- lib/view/ui/mypage/settings.dart
+++ lib/view/ui/mypage/settings.dart
@@ -257,7 +257,7 @@ class _SettingsState extends State<Settings> {
                     ),
                   ),
                 ),
-                _updateButton()
+                //_updateButton()
               ],
             ),
           ],
