46a687c31073404a461eb29eb2649bf050991518
siennaJeong <sienna4342@gmail.com>
2021-01-20 13:50:49 +0900

    [FIX] 210120 - Firebase Analytics 디버그 모드에서 적용 안되게.

--- lib/common/analytics/analytics_service.dart
+++ lib/common/analytics/analytics_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:device_info/device_info.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:package_info/package_info.dart';
 
 class AnalyticsService {
@@ -34,47 +35,51 @@ class AnalyticsService {
   setUserId(String userId) => this.userId = userId;
 
   Future<void> setUserProperties(String userId, String gender, String birthDate) async {
-    setUserId(userId);
-    await firebaseAnalytics.setUserId(userId);
-    await firebaseAnalytics.setUserProperty(name: 'gender', value: gender);
-    await firebaseAnalytics.setUserProperty(name: 'birthDate', value: birthDate);
+    if(!kDebugMode) {
+      setUserId(userId);
+      await firebaseAnalytics.setUserId(userId);
+      await firebaseAnalytics.setUserProperty(name: 'gender', value: gender);
+      await firebaseAnalytics.setUserProperty(name: 'birthDate', value: birthDate);
+    }
   }
 
   Future<void> sendAnalyticsEvent(bool isVisited, bool isPremium, String page, String click, String subPage, String value) async {
-    var now = DateTime.now();
-    packageInfo ??= await PackageInfo.fromPlatform();
+    if(!kDebugMode) {
+      var now = DateTime.now();
+      packageInfo ??= await PackageInfo.fromPlatform();
 
-    if(Platform.isIOS) {
-      iosDeviceInfo ??= await deviceInfo.iosInfo;
-      this.deviceName = iosDeviceInfo.model;
-      this.deviceModel = iosDeviceInfo.utsname.machine;
-      this.deviceOS = iosDeviceInfo.systemName;
-      this.deviceOSVersion = iosDeviceInfo.systemVersion;
-    }else {
-      androidDeviceInfo ??= await deviceInfo.androidInfo;
-      this.deviceName = androidDeviceInfo.model;
-      this.deviceModel = androidDeviceInfo.device;
-      this.deviceOS = "Android";
-      this.deviceOSVersion = androidDeviceInfo.version.release;
-    }
-
-    await firebaseAnalytics.logEvent(
-      name: "$PREFIX${Platform.isIOS ? 'ios' : 'and'}",
-      parameters: <String, String>{
-        'log_type': isVisited ? 'visit' : 'action',
-        'reg_date': '$now',
-        'user_id': this.userId,
-        'premium': isPremium ? 'premium' : 'free',
-        'device_model': this.deviceModel,
-        'device_name': this.deviceName,
-        'device_os' : this.deviceOS,
-        'device_os_version': this.deviceOSVersion,
-        'deukki_version': packageInfo.version,
-        'view': page,
-        'click': click,
-        'sub': subPage,
-        'value': value
+      if(Platform.isIOS) {
+        iosDeviceInfo ??= await deviceInfo.iosInfo;
+        this.deviceName = iosDeviceInfo.model;
+        this.deviceModel = iosDeviceInfo.utsname.machine;
+        this.deviceOS = iosDeviceInfo.systemName;
+        this.deviceOSVersion = iosDeviceInfo.systemVersion;
+      }else {
+        androidDeviceInfo ??= await deviceInfo.androidInfo;
+        this.deviceName = androidDeviceInfo.model;
+        this.deviceModel = androidDeviceInfo.device;
+        this.deviceOS = "Android";
+        this.deviceOSVersion = androidDeviceInfo.version.release;
       }
-    );
+
+      await firebaseAnalytics.logEvent(
+          name: "$PREFIX${Platform.isIOS ? 'ios' : 'and'}",
+          parameters: <String, String>{
+            'log_type': isVisited ? 'visit' : 'action',
+            'reg_date': '$now',
+            'user_id': this.userId,
+            'premium': isPremium ? 'premium' : 'free',
+            'device_model': this.deviceModel,
+            'device_name': this.deviceName,
+            'device_os' : this.deviceOS,
+            'device_os_version': this.deviceOSVersion,
+            'deukki_version': packageInfo.version,
+            'view': page,
+            'click': click,
+            'sub': subPage,
+            'value': value
+          }
+      );
+    }
   }
 }
