58cd092cac5c92311f92c7faa1aa1433720e28b6
siennajeong <sienna4342@gmail.com>
2021-02-24 15:11:21 +0900

    [FIX] 210224 - My -> Bookmark Score 없을때 별 아이콘 안나타나도록 변경

--- lib/view/ui/mypage/bookmark.dart
+++ lib/view/ui/mypage/bookmark.dart
@@ -71,25 +71,6 @@ class _BookMarkState extends State<BookMark> {
   }
 
   Widget _listItemWidget(int index, double deviceWidth, double deviceHeight) {
-    String firstStar, secondStar, thirdStar;
-    if(_bookmarkList[index].score == 1) {
-      firstStar = AppImages.fullStar;
-      secondStar = AppImages.emptyStar;
-      thirdStar = AppImages.emptyStar;
-    }else if(_bookmarkList[index].score == 2) {
-      firstStar = AppImages.fullStar;
-      secondStar = AppImages.fullStar;
-      thirdStar = AppImages.emptyStar;
-    }else if(_bookmarkList[index].score == 3) {
-      firstStar = AppImages.fullStar;
-      secondStar = AppImages.fullStar;
-      thirdStar = AppImages.fullStar;
-    }else {
-      firstStar = AppImages.emptyStar;
-      secondStar = AppImages.emptyStar;
-      thirdStar = AppImages.emptyStar;
-    }
-
     return GestureDetector(
       child: Card(
         color: Colors.white,
@@ -124,16 +105,7 @@ class _BookMarkState extends State<BookMark> {
                   ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 76),
-                child: Row(
-                  children: <Widget>[
-                    Image.asset(firstStar, width: 20,),
-                    Image.asset(secondStar, width: 20,),
-                    Image.asset(thirdStar, width: 20,),
-                  ],
-                ),
-              ),
+              _scoreWidget(_bookmarkList[index].score),
             ],
           ),
         ),
@@ -167,6 +139,41 @@ class _BookMarkState extends State<BookMark> {
         }
       },
     );
+  }
+
+  Widget _scoreWidget(int score) {
+    String firstStar, secondStar, thirdStar;
+    if(score != null) {
+      switch(score) {
+        case 1:
+          firstStar = AppImages.fullStar;
+          secondStar = AppImages.emptyStar;
+          thirdStar = AppImages.emptyStar;
+          break;
+        case 2:
+          firstStar = AppImages.fullStar;
+          secondStar = AppImages.fullStar;
+          thirdStar = AppImages.emptyStar;
+          break;
+        case 3:
+          firstStar = AppImages.fullStar;
+          secondStar = AppImages.fullStar;
+          thirdStar = AppImages.fullStar;
+          break;
+      }
+      return Container(
+        margin: EdgeInsets.only(left: 76),
+        child: Row(
+          children: <Widget>[
+            Image.asset(firstStar, width: 20,),
+            Image.asset(secondStar, width: 20,),
+            Image.asset(thirdStar, width: 20,),
+          ],
+        ),
+      );
+    }else {
+      return Container();
+    }
   }
 
   Widget _noListWidget() {
