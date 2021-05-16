0d5c5f51dea11ad6c90252e481b7e93d59f8eb57
siennajeong <sienna4342@gmail.com>
2021-05-16 22:44:06 +0900

    [ADD] 210516 - 카드형 가이드 진행중.

--- lib/view/ui/tutorial.dart
+++ lib/view/ui/tutorial.dart
@@ -1,8 +1,11 @@
 import 'package:deukki/common/utils/route_util.dart';
 import 'package:deukki/data/service/signin/auth_service_adapter.dart';
+import 'package:deukki/view/values/app_images.dart';
+import 'package:deukki/view/values/colors.dart';
 import 'package:deukki/view/values/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
 import 'package:provider/provider.dart';
 
 class Tutorial extends StatefulWidget {
@@ -12,11 +15,20 @@ class Tutorial extends StatefulWidget {
 
 class _TutorialState extends State<Tutorial> {
   final PageController _pageController = PageController(initialPage: 0);
+  final _currentPageNotifier = ValueNotifier<int>(0);
   AuthServiceAdapter authServiceAdapter;
   double _deviceWidth, _deviceHeight;
 
   String _buttonText = Strings.skip_btn;
-  List<Color> testColor = [Colors.blue, Colors.amber, Colors.green, Colors.deepPurpleAccent];
+  List<String> guideImages = [
+    AppImages.guide_1,
+    AppImages.guide_2,
+    AppImages.guide_3,
+    AppImages.guide_4,
+    AppImages.guide_5,
+    AppImages.guide_6,
+    AppImages.guide_7
+  ];
 
   @override
   void initState() {
@@ -28,57 +40,33 @@ class _TutorialState extends State<Tutorial> {
     return PageView.builder(
       controller: _pageController,
       onPageChanged: _setCurrentPage,
-      itemCount: testColor.length,
+      itemCount: guideImages.length,
       itemBuilder: (BuildContext context, int index) {
         return SizedBox.expand(
           child: Container(
-            color: testColor[index],
-            child: Center(
-              child: Text("pages : $index"),
-            ),
+            child: Image.asset(guideImages[index], fit: BoxFit.fitHeight,),
           ),
         );
       },
     );
   }
 
-  Widget _skipButtonWidget() {
+  Widget _indicatorWidget() {
     return Container(
-      margin: EdgeInsets.only(bottom: 40, left: _deviceWidth * 0.32, right: _deviceWidth * 0.32),
-      child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.all(14),
-            primary: Colors.transparent,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-                side: BorderSide(
-                    color: Colors.white,
-                    width: 2.0
-                )
-            ),
-          ),
-          child: Text(
-            _buttonText,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontFamily: "TmoneyRound",
-                fontWeight: FontWeight.w700
-            ),
-          ),
-          onPressed: () => { _exitTutorial() }
+      margin: EdgeInsets.only(bottom: 27),
+      child: CirclePageIndicator(
+        currentPageNotifier: _currentPageNotifier,
+        itemCount: guideImages.length,
+        dotColor: Colors.white,
+        selectedDotColor: MainColors.green_30,
+        dotSpacing: 15,
       ),
     );
   }
 
   void _setCurrentPage(int pages) {
     setState(() {
-      if(pages >= 3) {
-        _buttonText = Strings.start_btn;
-      }else {
-        _buttonText = Strings.skip_btn;
-      }
+      _currentPageNotifier.value = pages;
     });
   }
 
@@ -100,7 +88,7 @@ class _TutorialState extends State<Tutorial> {
         child: Stack(
           children: <Widget>[
             Positioned.fill(child: _pageViewWidget()),
-            Positioned(bottom: 0, left: 0, right: 0, child: _skipButtonWidget()),
+            Positioned(bottom: 0, left: 0, right: 0, child: _indicatorWidget()),
           ],
         ),
       ),
